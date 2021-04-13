36a8c898fab720eeb1555feba3d3f72a35873ce0
bryanadi02 <adachiyeon02@gmail.com>
2021-04-13 22:30:03 +0700

    soal no 5

---
+++ PersonFile/src/personfile/PersonFile.java
@@ -0,0 +1,10 @@
+package personfile;
+
+public class PersonFile {
+  
+    public static void main(String[] args) {
+       PreferredCustomer preferredCustomer = new PreferredCustomer("Brodin Sumargo","Embong Wetan",8888,02020,true,2000);
+       System.out.println(preferredCustomer.toString());
+    }
+    
+}
